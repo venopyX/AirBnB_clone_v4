ddd9ddbe472db03698d09818a9168640ebddb400
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-06-18 09:01:27 +0100

    task 1

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def close_db(error):
-    """ Remove the current SQLAlchemy Session """
+    """ Remove/Close the current SQLAlchemy Session """
     storage.close()
 
 
