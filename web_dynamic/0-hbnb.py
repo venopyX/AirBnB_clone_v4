3176cd28f69229c2b0fe0b0776070609881ba26e
Suntha <Suntha.Lucas@atlasschool.com>
2024-08-04 13:30:05 -0500

    fixed error on flask import

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,33 +1,48 @@
 #!/usr/bin/python3
-from flask import Flask
-from flask import render_template
-from flask import g
+
+"""
+Flask App that integrates with AirBnB static HTML Template
+"""
+from flask import Flask, render_template, url_for
 from models import storage
-from models.state import State
-from models.amenity import Amenity
-from models.place import Place
-"""
-Script that starts a Flask web application
-"""
+import uuid;
+
+# flask setup
 app = Flask(__name__)
+app.url_map.strict_slashes = False
+port = 5000
+host = '0.0.0.0'
 
 
+# begin flask page rendering
 @app.teardown_appcontext
-def teardown_data(self):
-        storage.close()
+def teardown_db(exception):
+    """
+    after each request, this method calls .close() (i.e. .remove()) on
+    the current SQLAlchemy Session
+    """
+    storage.close()
 
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """ return all states in the db  """
-    states = storage.all(State).values()
-    amenities = storage.all(Amenity).values()
-    places = storage.all(Place).values()
-    print(places)
-    return render_template('100-hbnb.html',
+@app.route('/0-hbnb')
+def hbnb_filters(the_id=None):
+    """
+    handles request to custom template with states, cities & amentities
+    """
+    state_objs = storage.all('State').values()
+    states = dict([state.name, state] for state in state_objs)
+    amens = storage.all('Amenity').values()
+    places = storage.all('Place').values()
+    users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
+                 for user in storage.all('User').values())
+    return render_template('0-hbnb.html',
+                           cache_id=uuid.uuid4(),
                            states=states,
+                           amens=amens,
                            places=places,
-                           amenities=amenities)
+                           users=users)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    """
+    MAIN Flask App"""
+    app.run(host=host, port=port)
