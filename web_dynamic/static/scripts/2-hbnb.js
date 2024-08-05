330f794a46de6ae0915f281d88462391da3d54d9
Suntha <Suntha.Lucas@atlasschool.com>
2024-08-05 09:22:50 -0500

    added div to the element

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,36 +1,25 @@
-$(document).ready(function() {
-    // Object to keep track of selected amenities
-    var checkedAmenities = {};
-
-    // Function to update the API status indicator
-    function updateApiStatus() {
-        $.get('http://0.0.0.0:5001/api/v1/status/', function(data) {
-            if (data.status === 'OK') {
-                $('#api_status').addClass('available').removeClass('unavailable');
-            } else {
-                $('#api_status').addClass('unavailable').removeClass('available');
-            }
-        }).fail(function() {
-            $('#api_status').addClass('unavailable').removeClass('available');
-        });
-    }
-
-    // Call the API status function on page load
-    updateApiStatus();
-
-    // Event handler for checkbox changes
-    $('input[type="checkbox"]').change(function() {
-        const amenityId = $(this).data('id');
-        const amenityName = $(this).data('name');
-
-        if ($(this).is(':checked')) {
-            checkedAmenities[amenityId] = amenityName;
-        } else {
-            delete checkedAmenities[amenityId];
-        }
-
-        // Update the display
-        const $h4 = $('div.amenities h4');
-        $h4.text(Object.values(checkedAmenities).join(', '));
+$(document).ready(function () {
+    let checkedAmenities = {};
+    $(document).on('change', "input[type='checkbox']", function () {
+      if (this.checked) {
+        checkedAmenities[$(this).data('id')] = $(this).data('name');
+      } else {
+        delete checkedAmenities[$(this).data('id')];
+      }
+      let lst = Object.values(checkedAmenities);
+      if (lst.length > 0) {
+        $('div.amenities > h4').text(Object.values(checkedAmenities).join(', '));
+      } else {
+        $('div.amenities > h4').html('&nbsp;');
+      }
     });
-});
+    $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
+      if (textStatus === 'success') {
+        if (data.status === 'OK') {
+          $('#api_status').addClass('available');
+        } else {
+          $('#api_status').removeClass('available');
+        }
+      }
+    });
+  });
