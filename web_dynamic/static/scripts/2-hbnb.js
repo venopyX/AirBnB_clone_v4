a2f4931ae6322d1c0b0589448e3d0cfe54d5f04a
Josh Merrick <moshjerrick@gmail.com>
2024-08-05 14:51:29 -0500

    built 2-hbnb.js off of 1-hbnb.js

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,3 +1,21 @@
+$(document).ready(function() {
+    var checkedAmenities = {};
+
+    $('input[type="checkbox"]').change(function() {
+        const amenityId = $(this).data('id');
+        const amenityName = $(this).data('name');
+
+        if ($(this).is(':checked')) {
+            checkedAmenities[amenityId] = amenityName;
+        } else {
+            delete checkedAmenities[amenityId];
+        }
+
+        // Update the display
+        const $h4 = $('div.amenities h4');
+        $h4.text(Object.values(checkedAmenities).join(', '));
+    });
+});
 document.addEventListener('DOMContentLoaded', function () {
   const apiStatusDiv = document.getElementById('api_status');
 
