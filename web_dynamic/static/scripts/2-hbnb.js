c57bda61501d79e93f8b9047b3f198c71ce72224
Suntha <Suntha.Lucas@atlasschool.com>
2024-08-05 09:43:53 -0500

    added http 0000 5001

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,25 +1,23 @@
-$(document).ready(function () {
-    let checkedAmenities = {};
-    $(document).on('change', "input[type='checkbox']", function () {
-      if (this.checked) {
-        checkedAmenities[$(this).data('id')] = $(this).data('name');
-      } else {
-        delete checkedAmenities[$(this).data('id')];
-      }
-      let lst = Object.values(checkedAmenities);
-      if (lst.length > 0) {
-        $('div.amenities > h4').text(Object.values(checkedAmenities).join(', '));
-      } else {
-        $('div.amenities > h4').html('&nbsp;');
-      }
-    });
-    $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
-      if (textStatus === 'success') {
-        if (data.status === 'OK') {
-          $('#api_status').addClass('available');
-        } else {
-          $('#api_status').removeClass('available');
-        }
-      }
-    });
-  });
+document.addEventListener('DOMContentLoaded', function () {
+  const apiStatusDiv = document.getElementById('api_status');
+
+  function updateApiStatus() {
+      fetch('http://0.0.0.0:5001/api/v1/status/')
+          .then(response => response.json())
+          .then(data => {
+              if (data.status === 'OK') {
+                  apiStatusDiv.classList.add('available');
+              } else {
+                  apiStatusDiv.classList.remove('available');
+              }
+          })
+          .catch(() => {
+              apiStatusDiv.classList.remove('available');
+          });
+  }
+
+  updateApiStatus();
+  // Optionally, you can update the status periodically
+  setInterval(updateApiStatus, 5000); // Check every 5 seconds
+});
+
