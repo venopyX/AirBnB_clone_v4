af2a99062a5d5207eb16d592d856e33cbe31e590
Solomon Nwante <myphilothea@gmail.com>
2024-08-09 13:06:53 +0100

    Implement filters section

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,4 +1,12 @@
-const inputs = $('input');
-
-inputs.each(function () {}
-$('document').ready(function () {})
+$('document').ready(function () {
+  let amenities = {}
+  $('INPUT[type="checkbox"]').change( function () {
+    if ($(this).is(':checked')) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    }
+    else {
+      delete amenities[$(this).attr('data-id')];
+    }
+    $('.amenities H4').text(Object.values(amenities).join(', '));
+  });
+});
